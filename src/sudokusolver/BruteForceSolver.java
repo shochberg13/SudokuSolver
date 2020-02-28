0029e403ae359c6b008cb0e8e6c46e92816a7ac0
Seth Hochberg <shochberg13@gmail.com>
2020-02-28 15:18:54 -0500

    Removed unnecessary methods (check if row, col are solved)

--- src/sudokusolver/BruteForceSolver.java
+++ src/sudokusolver/BruteForceSolver.java
@@ -1,7 +1,5 @@
 package sudokusolver;
 
-import java.util.HashSet;
-
 public class BruteForceSolver {
 	private int[][] grid;
 	private boolean[][] userInput;
@@ -158,51 +156,6 @@ public class BruteForceSolver {
 			}
 		}
 		return true;
-	}
-	
-	public boolean isRowSolved(int row){
-		HashSet<Integer> repeatCheck = new HashSet<Integer>();
-		for (int i = 0; i < 9; i++){
-			int val = grid[row][i];
-			if (val > 0 && val <= 9) repeatCheck.add(val);
-		}
-		if (repeatCheck.size() == 9) return true;
-		return false;
-	}
-	
-	public boolean isColSolved(int col){
-		HashSet<Integer> repeatCheck = new HashSet<Integer>();
-		for (int i = 0; i < 9; i++){
-			int val = grid[i][col];
-			if (val > 0 && val <= 9) repeatCheck.add(val);
-		}
-		
-		if (repeatCheck.size() == 9) return true;
-		return false;
-	}
-	
-	public boolean isSmallBoxSolved(int box){
-		// @param box: 1 for top left, 2 top center, ... , 9 for bottom right
-		
-		// Convert box number into top left coordinate of the box
-		int rowStart = 1;
-		if (box < 4) rowStart = 0;
-		if (box > 6) rowStart = 2;
-		
-		int colStart = 0;
-		if (box % 3 == 2) colStart = 3;
-		if (box % 3 == 0) colStart = 6;
-		
-		// Check for repeats
-		HashSet<Integer> repeatCheck = new HashSet<Integer>();
-		for (int i = rowStart; i < 3; i++){
-			for(int j = colStart; j < 3; j++){
-				repeatCheck.add(grid[i][j]);
-			}
-		}
-		
-		if (repeatCheck.size() == 9) return true;
-		return false;
 	}
 
 	public boolean isBoardComplete(){
